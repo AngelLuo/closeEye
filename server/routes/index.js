bd38124ca6e5024a5c56526f4b52c9e3a219f305
lh_wang <lh_wang@Ctrip.com>
2015-10-29 10:27:50 +0800

    增加进入房间的api

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,15 @@ var FILE_NAME = './db.json';
 
 /*创建房间*/
 router.get('/create', function(req, res, next) {
+  //总人数
   var all_counts = parseInt(req.param('all_counts'));
+  //警察人数
   var police_counts = parseInt(req.param('police_counts'));
+  //杀手人数
   var killer_counts = parseInt(req.param('killer_counts'));
-
+  //设备ID
+  var client_id = req.param('client_id');
+  //数据表
   var db = null;
   //读取数据
   try{
@@ -23,10 +28,10 @@ router.get('/create', function(req, res, next) {
 
   var num = '';
   //生成房间号
-  var room_num = randomNum(num, db);
+  var room_num = randomRoomNum(num, db);
   //生成身份数组
-  var genPeople = genPeople(all_counts, police_counts, killer_counts);
-  if(!genPeople){
+  var gen = genPeople(all_counts, police_counts, killer_counts);
+  if(!gen){
     return res.send({
       status: 0,
       info: '人数有问题'
@@ -34,18 +39,20 @@ router.get('/create', function(req, res, next) {
   }
 
   var room = {
+    "create_id": client_id,
     "room_num": room_num,
     "is_over": "false",
     "all_counts": all_counts,
-    "police": genPeople.policeArr,
-    "killer": genPeople.killerArr
+    "police": gen.policeArr,
+    "killer": gen.killerArr,
+    "people":[]
   };
 
   db.push(room);
   //写入到db.json
   try{
     fs.writeFileSync(FILE_NAME, JSON.stringify(db));
-    return res.send(room);
+    return res.send({status:1, room: room});
   }catch(e){
     return res.send({
       status: 0,
@@ -55,14 +62,17 @@ router.get('/create', function(req, res, next) {
 
 });
 
-/*几号进哪个房间*/
-router.get('/room_num', function(req, res, next) {
-  return res.send(genPeople(6, 1, 1));
-});
 
+/**
+ * 产生可用房间号
+ *
+ * @param {number} num
+ * @param {object} db
+ * @return {number}
+ * @api private
+ */
 
-//产生可用房间号
-function randomNum(num, db){
+function randomRoomNum(num, db){
   //生成随即数
   num = '';
   for(var i = 0; i < 4; i++){
@@ -83,7 +93,15 @@ function randomNum(num, db){
   return num;
 }
 
-//生成警察、杀手、平民
+/**
+ * 生成警察、杀手、平民
+ *
+ * @param {number} allCounts
+ * @param {number} policeCounts
+ * @param {number} killerCounts
+ * @return {object}
+ * @api private
+ */
 function genPeople(allCounts, policeCounts, killerCounts){
   if(policeCounts >= allCounts || killerCounts >= allCounts || (policeCounts + killerCounts) >= allCounts){
     return null; /*人数不对，返回null对象*/
@@ -109,7 +127,17 @@ function genPeople(allCounts, policeCounts, killerCounts){
 }
 
 
-/*生成随机的身份数组*/
+/**
+ * 生成随机的身份数组
+ *
+ * @param {number} allCounts
+ * @param {number} typeCounts
+ * @param {array} arr
+ * @param {string} type
+ * @param {array} policeArr
+ * @param {array} killerArr
+ * @api private
+ */
 function randomNum(allCounts, typeCounts, arr, type, policeArr, killerArr){
   for(var i = 0; i < typeCounts; i++){
     var num = randomN(allCounts, arr);
@@ -124,6 +152,14 @@ function randomNum(allCounts, typeCounts, arr, type, policeArr, killerArr){
 }
 
 
+/**
+ * 生成不在数组里的随机数
+ *
+ * @param {number} allCounts
+ * @param {array} arr
+ * @return {object}
+ * @api private
+ */
 function randomN(allCounts, arr){
   var num = parseInt(Math.random() * allCounts);
   for(var j = 0; j < arr.length; j++){
@@ -134,6 +170,74 @@ function randomN(allCounts, arr){
   }
   return num;
 }
+
+
+
+
+/*几号进哪个房间*/
+router.get('/room_num', function(req, res, next) {
+  //房间号
+  var room_num = req.param('room_num');
+  //设备ID
+  var client_id = req.param('client_id');
+  //数据表
+  var db = null;
+  //读取数据
+  try{
+    db = JSON.parse(fs.readFileSync(FILE_NAME).toString());
+  }catch(e){
+    return res.send({
+      status: 0,
+      info:'读取数据失败'
+    })
+  }
+
+  for(var i in db){
+    if(db[i].room_num === room_num){
+
+      //如果存在client_id，则直接返回号码和身份
+      var room = db[i];
+      for(var j in room.people){
+        if(room.people[j] === client_id){
+          //返回当前用户的编号
+          return res.send({status:1, num: j});
+        }
+      }
+
+      //房主，返回所有信息
+      if(room.create_id === client_id ){
+        delete room.people;
+        return res.send({status:1, room: room});
+      }
+
+      //房间人数已满，拒绝进入
+      if(room.people.length >= room.all_counts){
+        return res.send({
+          status: 0,
+          info: '房间人数已满'
+        });
+      }
+      //如果不存在，直接push到数组
+      room.people.push(client_id);
+
+      try{
+        //写入到db.json
+        fs.writeFileSync(FILE_NAME, JSON.stringify(db));
+        //返回第一次进入的编号
+        return res.send({status:1, num: room.people.length - 1});
+      }catch(e){
+        return res.send({status: 0});
+      }
+    }
+  }
+
+  return {
+    status: 0,
+    info: '该房间已过期'
+  };
+
+
+});
 
 module.exports = router;
 
